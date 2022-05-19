b1524fcf73c2cda52e875eeb3909d5291179b176
maheendran kp <maheendrankp100@gmail.com>
2022-05-19 20:02:11 +0530

    React file added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import Answer1 from './components/Answer1'; 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+   let info=[{Heading:"Mobile Operating System",P1:"Android",P2:"Blackberry",P3:"iPhone",P4:"Windows Phone"},
+   {Heading:"Mobile Manufacturers",P1:"Samsung",P2:"HTC",P3:"Micromax",P4:" Apple"}
+  ]
+       
+   return ( 
+       <div className='App'>
+           <Answer1 {...info[0]}/>
+           <Answer1 {...info[1]}/>
+       </div>
   );
 }
 
---
+++ src/components/Answer1.jsx
@@ -0,0 +1,18 @@
+import React from"react"
+
+const Answer1=({Heading,P1,P2,P3,P4})=>{
+    return <div>
+        <h1>{Heading}</h1>
+        <ul>
+          <li>{P1}</li>
+          <li>{P2}</li>
+          <li>{P3}</li>
+          <li>{P4}</li>
+         
+      </ul>
+        </div>
+         
+      
+   
+}
+export default Answer1
